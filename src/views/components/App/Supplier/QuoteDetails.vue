47d3d39aa580f5f5203bfd6e3acbcc5b3a53bbc7
Tawing dela Rama <yabuking84@gmail.com>
2019-03-06 09:58:42 +0400

    u

--- src/views/components/App/Supplier/QuoteDetails.vue
+++ src/views/components/App/Supplier/QuoteDetails.vue
@@ -4,15 +4,18 @@
 		<v-toolbar color="grey darken-4" class="white--text" height="40px">
         	<v-toolbar-title class="subheading font-weight-light">Quote Details</v-toolbar-title>
         	<v-spacer></v-spacer>
-        	<v-toolbar-title class="subheading font-weight-light">Your Current Quote is $11.50</v-toolbar-title>
+        	
+        	<v-toolbar-title class="subheading font-weight-light" v-if="hasBid">
+        		Your Current Quote is ${{ bid.total_price }}
+        	</v-toolbar-title>
 
 			<v-spacer></v-spacer>
 
 			<v-toolbar-title @click="bidDialog=true" class="subheading font-weight-light pa-2">
              	
              	<v-btn 
-             		v-if="editButton" 
-             		@click="QuoteDialog = true" 
+             		v-if="hasBid"
+             		@click="openQuoteDialog = true" 
              		class="font-weight-light" 
              		color="blue" 
              		dark 
@@ -23,7 +26,7 @@
 
              	<v-btn 
              		v-else 
-             		@click="QuoteDialog = true" 
+             		@click="openQuoteDialog = true" 
              		class="font-weight-light" 
              		color="green" 
              		dark 
@@ -34,7 +37,6 @@
 
 			</v-toolbar-title>
 
-
       	</v-toolbar>
 
       	<v-card class="minh-500" color="grey lighten-5">
@@ -43,16 +45,19 @@
 
 			<!-- xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -->
 			<!-- xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -->
-				<v-flex xs5>					
+				<v-flex xs5 v-if="hasBid">
 					<image-gallery-small></image-gallery-small>
 				</v-flex>
 
-
-				<v-flex xs7>
+				<v-flex xs7 v-if="hasBid">
 		  	  		<v-container>
 			  	  		<v-flex xs12>
 			  	  			<h5 class="font-weight-thin">Product name</h5>
-			  	  			<h2>Super Lighting Bulb Infinity</h2>
+			  	  			<h2>{{ bid.product_name }}</h2>
+				        </v-flex>
+
+			  	  		<v-flex xs12 pt-0>
+			  	  			<h3 class="font-weight-regular">{{ bid.description }}</h3>
 				        </v-flex>
 
 			  	  		<v-flex xs12>
@@ -60,29 +65,42 @@
 
 				           	<v-flex xs4 pa-2>
 				  	  			<h5 class="font-weight-thin">Quantity</h5>
-				  	  			<h3>100 pcs</h3>
+				  	  			<h3>{{ inquiry.quantity }} pcs</h3>
 					        </v-flex>
 					        
 					        <v-flex xs4 pa-2>					          	
 				  	  			<h5 class="font-weight-thin">Unit Price</h5>
-				  	  			<h3>$11.50</h3>
+				  	  			<h3>${{ bid.price }}</h3>
 					        </v-flex>
 				        
 				      		<v-flex xs4 pa-2>
 				  	  			<h5 class="font-weight-thin">Total Price</h5>
-				  	  			<h2>$1150.00</h2>					          	
+				  	  			<h2>${{ bid.total_price }}</h2>					          	
 					        </v-flex>
 				        </v-layout>
 				        </v-flex>
 
 				         <v-flex xs12>					        
-				  	  		<h5 class="font-weight-thin">Message</h5>
+				  	  		<h5 class="font-weight-thin">Remarks</h5>
 				  	  		<h3>
-				  	  			<p>lorew ipsurew ipsurew ipsuipsurew ipsuipsurew ipsurew ipsum </p>				  	  			
+				  	  			<p>{{ bid.remarks }}</p>
 				  	  		</h3>
 				      	 </v-flex>
 		  	  		</v-container>
+
 				</v-flex>
+
+				<v-flex xs12 v-if="!hasBid">
+					<v-alert 
+						:value="true"
+						type="warning" 
+						style="width: auto;"
+						class="ma-4"
+						outline>
+						No Quote yet..
+					</v-alert>
+				</v-flex>
+
 			<!-- xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -->
 			<!-- xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -->
 
@@ -93,7 +111,7 @@
 	    </v-card>
 		
 		<span>
-			<quote-dialog :QuoteDialog.sync="QuoteDialog" :bidAdded.sync="bidAdded" :inquiry="inquiry"></quote-dialog>
+			<quote-dialog :openQuoteDialog.sync="openQuoteDialog" :inquiry="inquiry"></quote-dialog>
 		</span>
 
 </div>
@@ -105,6 +123,7 @@ import ImageGallerySmall from "@/views/Components/App/ImageGallerySmall"
 
 import InquiryDialog from "@/views/Components/App/Buyer/BuyerInquiryViewDialog"
 import QuoteDialog from "@/views/Components/App/Supplier/QuoteDialog"
+import inqEvntBs from "@/bus/inquiry";
 
 export default {
 
@@ -118,29 +137,59 @@ export default {
 
     data: ()=>({
 		bidDialog:false,
-		QuoteDialog:false,
-		bid: null,
+		openQuoteDialog:false,
+		bid: {
+			price: null,
+			total_price: null,
+			product_name: null,
+			remarks: null,
+			description: null,
+		},
 		editButton: false,
+		hasBid: false,
 		bidAdded: false,
 	}),
 
+	methods:{
+		reloadBid(){
+			this.$store.dispatch('spplrInq/getInquiryBid_a',{inq_id:this.inquiry.id})
+			.then((data)=>{
+				this.bid = data;
+
+				// console.log("this.bid");
+				// console.log(this.bid);
+
+				// check if  already has bid
+				this.hasBid = (this.bid)?true:false;
+
+			})
+			.catch((error)=>{
+				console.log(error);
+			});
+
+		},
+	},
+
+	watch: {
+
+		inquiry:{			
+			handler(nVal,oVal){
+				this.reloadBid();
+			},
+			deep: true,
+		}
+
+	},
 
 
 	created() {
 
-		this.$store.dispatch('spplrInq/getInquiryBid_a',{inq_id:this.inq_id})
-		.then((data)=>{
-			this.bid = data;
-			console.log(this.bid);
+		this.reloadBid();
 
-			// check if  already has bid
-			if(this.bid)
-			this.editButton = true;
-
-		})
-		.catch((error)=>{
-			console.log(error);
+		inqEvntBs.onBidFormSubmitted(()=>{
+			this.reloadBid();
 		});
+
 	
 	},
 
